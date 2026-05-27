9040d4b8b4d83063f51041701e1075f33f6d333c
hahnxk <andrewjh613@gmail.com>
2026-05-27 13:43:47 -0700

    Add: buy-and-sell-crypto - submission-18

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-18.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Idea: 
+        # Initialize profit as 0
+        # 1. Loop through prices
+        # 2. Initialize lowest cost (day 1 price) and compare profit to diff between day 
+        #    1 and following days.
+        # 3. If new day has lower cost than current lowest day, swap and go back to step 1.
+        # 4. Repeat.
+
+        profit = 0
+        lowest_price = prices[0]
+        for price in prices:
+            if price < lowest_price:
+                lowest_price = price
+            profit = max(profit, price - lowest_price)
+        
+        return profit
